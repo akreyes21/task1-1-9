bbf51e2f30899b7907fec9878b8d85e5c2424881
akreyes21 <164187372+akreyes21@users.noreply.github.com>
2024-04-18 11:29:04 +0800

    Task119.java

--- Main.java
+++ Main.java
@@ -1,1 +1,41 @@
+import static java.lang.Math.*;
 
+public class MathOperations {
+
+    public static int add(int a, int b) {
+        return addExact(a, b);
+    }
+
+    public static int subtract(int a, int b) {
+        return subtractExact(a, b);
+    }
+
+    public static int multiply(int a, int b) {
+        return multiplyExact(a, b);
+    }
+
+    public static float divide(int a, int b) {
+        return (float) a / b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        // Perform addition
+        int sum = add(num1, num2);
+        System.out.println("Sum: " + sum);
+
+        // Perform subtraction
+        int difference = subtract(num1, num2);
+        System.out.println("Difference: " + difference);
+
+        // Perform multiplication
+        int product = multiply(num1, num2);
+        System.out.println("Product: " + product);
+
+        // Perform division
+        int quotient = divide(num1, num2);
+        System.out.println("Quotient: " + quotient);
+    }
+}
